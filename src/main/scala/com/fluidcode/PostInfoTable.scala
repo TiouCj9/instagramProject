31ef37316584653c0dc00ba116d8c79be46b508c
Youssef <ybenmuhamed@icloud.com>
2022-04-25 11:31:39 +0100

    PostInfoTable unit test successfull

--- src/main/scala/com/fluidcode/PostInfoTable.scala
+++ src/main/scala/com/fluidcode/PostInfoTable.scala
@@ -11,20 +11,21 @@ object PostInfoTable {
       explode(col("GraphImages")).as("GraphImages")
     )
   }
-
-  def getEdgeMediaCaptionData(rawData: DataFrame): DataFrame = {
-    getStructGraphImages(rawData).select(
+  def getCommentsInfoData(RawData: DataFrame): DataFrame={
+    getStructGraphImages(RawData).select(
       col("GraphImages").getField("comments_disabled").alias("comments_disabled").cast("Boolean"),
       col("GraphImages").getField("dimensions").getField("height").alias("dimensions_height").cast("Long"),
       col("GraphImages").getField("dimensions").getField("width").alias("dimensions_width").cast("Long"),
       col("GraphImages").getField("display_url").alias("display_url").cast("String"),
       col("GraphImages").getField("edge_media_preview_like").getField("count").alias("edge_media_preview_like_count").cast("Long"),
-      explode(col("GraphImages.edge_media_to_caption.edges")).as("edge_media_to_caption")
+      explode(col("GraphImages.edge_media_to_caption.edges").as("edge_media_to_caption_edges"))
     )
   }
-  def getTagsData(Tags: DataFrame): DataFrame ={
-    getTagsData(Tags).select(
-      col("edge_media_to_caption").getField("node").getField("text").alias("edge_media_to_caption_edges_node_text").cast("String"),
+
+
+  def getEdgeMediaCaptionData(rawData: DataFrame): DataFrame = {
+    getCommentsInfoData(rawData).select(
+      col("edge_media_to_caption_edges").getField("node").getField("count").alias("edge_media_to_caption_edges_node_text").cast("String"),
       col("GraphImages").getField("edge_media_to_comment").getField("count").alias("edge_media_to_comment_count").cast("Long"),
       col("GraphImages").getField("gating_info").alias("gating_info").cast("String"),
       col("GraphImages").getField("id").alias("id").cast("String"),
@@ -36,24 +37,25 @@ object PostInfoTable {
       explode(col("GraphImages.tags")).as("tags")
     )
   }
+
   def getThumbnail(thumbn: DataFrame): DataFrame ={
-    getThumbnail(thumbn).select(
+    getEdgeMediaCaptionData(thumbn).select(
       col("tags").alias("tags").cast("String"),
       col("GraphImages").getField("taken_at_timestamp").alias("taken_at_timestamp").cast("Long"),
       explode(col("GraphImages.thumbnail_resources")).as("thumbnail_resources")
     )
   }
   def getUrlsData(Urls: DataFrame): DataFrame ={
-    getUrlsData(Urls).select(
-      col("thumbnail_resources").getField("config_height").alias("config_height").cast("Long"),
-      col("thumbnail_resources").getField("config_width").alias("config_width").cast("Long"),
-      col("thumbnail_resources").getField("src").alias("src").cast("String"),
+    getThumbnail(Urls).select(
+      col("thumbnail_resources").getField("config_height").alias("thumbnail_resources_config_height").cast("Long"),
+      col("thumbnail_resources").getField("config_width").alias("thumbnail_resources_config_width").cast("Long"),
+      col("thumbnail_resources").getField("src").alias("thumbnail_resources_src").cast("String"),
       explode(col("GraphImages.urls")).as("urls")
     )
   }
 
   def getPostInfoTable(Raw: DataFrame) : DataFrame = {
-    getPostInfoTable(Raw).select(
+    getUrlsData(Raw).select(
       col("comments_disabled"),
       col("dimensions_height"),
       col("dimensions_width"),
@@ -70,9 +72,9 @@ object PostInfoTable {
       col("shortcode"),
       col("tags"),
       col("taken_at_timestamp"),
-      col("config_height"),
-      col("config_width"),
-      col("src"),
+      col("thumbnail_resources_config_height"),
+      col("thumbnail_resources_config_width"),
+      col("thumbnail_resources_src"),
       col("urls").alias("urls").cast("String"),
       col("GraphImages").getField("username").alias("username").cast("String")
     )
