380c98af84fd70573d12090f96a0fa2cc50eecfa
Youssef <ybenmuhamed@icloud.com>
2022-04-28 01:36:05 +0100

    All unit test for Tables has been successfully tested

--- src/main/scala/com/fluidcode/PostInfoTable.scala
+++ src/main/scala/com/fluidcode/PostInfoTable.scala
@@ -1,31 +1,21 @@
 package com.fluidcode
-
-import com.fluidcode.CommentsTable.getCommentsData
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{col, explode}
 
 object PostInfoTable {
 
-  def getStructGraphImages(GraphImages: DataFrame): DataFrame = {
-    GraphImages.select(
+  def getStructGraphImages(GraphImage: DataFrame): DataFrame = {
+    GraphImage.select(
       explode(col("GraphImages")).as("GraphImages")
     )
   }
   def getCommentsInfoData(RawData: DataFrame): DataFrame={
-    getStructGraphImages(RawData).select(
+    getStructGraphImages(RawData).select(  //select all struct columns
       col("GraphImages").getField("comments_disabled").alias("comments_disabled").cast("Boolean"),
       col("GraphImages").getField("dimensions").getField("height").alias("dimensions_height").cast("Long"),
       col("GraphImages").getField("dimensions").getField("width").alias("dimensions_width").cast("Long"),
       col("GraphImages").getField("display_url").alias("display_url").cast("String"),
       col("GraphImages").getField("edge_media_preview_like").getField("count").alias("edge_media_preview_like_count").cast("Long"),
-      explode(col("GraphImages.edge_media_to_caption.edges").as("edge_media_to_caption_edges"))
-    )
-  }
-
-
-  def getEdgeMediaCaptionData(rawData: DataFrame): DataFrame = {
-    getCommentsInfoData(rawData).select(
-      col("edge_media_to_caption_edges").getField("node").getField("count").alias("edge_media_to_caption_edges_node_text").cast("String"),
       col("GraphImages").getField("edge_media_to_comment").getField("count").alias("edge_media_to_comment_count").cast("Long"),
       col("GraphImages").getField("gating_info").alias("gating_info").cast("String"),
       col("GraphImages").getField("id").alias("id").cast("String"),
@@ -34,19 +24,75 @@ object PostInfoTable {
       col("GraphImages").getField("media_preview").alias("media_preview").cast("String"),
       col("GraphImages").getField("owner").getField("id").alias("owner_id").cast("String"),
       col("GraphImages").getField("shortcode").alias("shortcode").cast("String"),
-      explode(col("GraphImages.tags")).as("tags")
+      col("GraphImages").getField("taken_at_timestamp").alias("taken_at_timestamp").cast("Long"),
+      col("GraphImages").getField("username").alias("username").cast("String"),
+      explode(col("GraphImages.edge_media_to_caption.edges")).as("edges")
+      // select all array columns in raw state, without exploding
     )
   }
 
-  def getThumbnail(thumbn: DataFrame): DataFrame ={
-    getEdgeMediaCaptionData(thumbn).select(
+
+  def getEdgeMediaCaptionData(Data: DataFrame): DataFrame = {
+    getCommentsInfoData(Data).select( //select all the above + select structs from exploded column (example : edges)
+      col("comments_disabled"),
+      col("dimensions_height"),
+      col("dimensions_width"),
+      col("display_url"),
+      col("edge_media_preview_like_count"),
+      col("edge_media_to_comment_count"),
+      col("gating_info"),
+      col("id"),
+      col("is_video"),
+      col("location"),
+      col("media_preview"),
+      col("owner_id"),
+      col("shortcode"),
+      col("taken_at_timestamp"),
+      col("edges").getField("node").getField("text").alias("edge_media_to_caption_edges_node_text").cast("String"),
+      explode(col("GraphImages.tags")).as("tags")
+      // explode one of the remaining arrays
+    )
+  }
+
+  def getThumbnail(thumbn: DataFrame): DataFrame ={ //loop
+     getEdgeMediaCaptionData(thumbn).select(
+       col("comments_disabled"),
+       col("dimensions_height"),
+       col("dimensions_width"),
+       col("display_url"),
+       col("edge_media_preview_like_count"),
+       col("edge_media_to_comment_count"),
+       col("gating_info"),
+       col("id"),
+       col("is_video"),
+       col("location"),
+       col("media_preview"),
+       col("owner_id"),
+       col("shortcode"),
+       col("taken_at_timestamp"),
+       col("edge_media_to_caption_edges_node_text"),
       col("tags").alias("tags").cast("String"),
-      col("GraphImages").getField("taken_at_timestamp").alias("taken_at_timestamp").cast("Long"),
       explode(col("GraphImages.thumbnail_resources")).as("thumbnail_resources")
     )
   }
   def getUrlsData(Urls: DataFrame): DataFrame ={
     getThumbnail(Urls).select(
+      col("comments_disabled"),
+      col("dimensions_height"),
+      col("dimensions_width"),
+      col("display_url"),
+      col("edge_media_preview_like_count"),
+      col("edge_media_to_comment_count"),
+      col("gating_info"),
+      col("id"),
+      col("is_video"),
+      col("location"),
+      col("media_preview"),
+      col("owner_id"),
+      col("shortcode"),
+      col("taken_at_timestamp"),
+      col("edge_media_to_caption_edges_node_text"),
+      col("tags"),
       col("thumbnail_resources").getField("config_height").alias("thumbnail_resources_config_height").cast("Long"),
       col("thumbnail_resources").getField("config_width").alias("thumbnail_resources_config_width").cast("Long"),
       col("thumbnail_resources").getField("src").alias("thumbnail_resources_src").cast("String"),
@@ -76,7 +122,7 @@ object PostInfoTable {
       col("thumbnail_resources_config_width"),
       col("thumbnail_resources_src"),
       col("urls").alias("urls").cast("String"),
-      col("GraphImages").getField("username").alias("username").cast("String")
+      col("username")
     )
   }
 
--- src/test/scala/com/fluidcode/ProfileInfoTablaSpec.scala
+++ src/test/scala/com/fluidcode/ProfileInfoTablaSpec.scala
@@ -1,3 +1,5 @@
+package com.fluidcode
+
 import com.fluidcode.ProfileInfoTable._
 import org.apache.spark.sql.SparkSession
 import org.scalatest.GivenWhenThen
@@ -14,7 +16,7 @@ case class Data (biography: String, followers_count: Long, following_count: Long
 case class GraphProfileInfoData (GraphProfileInfo: RawData)
 
 class ProfileInfoTable extends AnyFlatSpec with Matchers with GivenWhenThen {
-  "getCommentsTable" should "extract comments data from raw data" in {
+  "getProfileInfoTable" should "extract comments data from raw data" in {
     val spark: SparkSession = SparkSession
       .builder()
       .master("local[*]")
@@ -30,7 +32,7 @@ class ProfileInfoTable extends AnyFlatSpec with Matchers with GivenWhenThen {
     When("getProfileInfo Is invoked")
     val ProfileTable = getProfileInfoTable(input)
 
-    Then("CommentTable should contain the same element as raw data")
+    Then("ProfileInfo should contain the same element as raw data")
     val expectedResult = Seq(
       Profile(1286323200,"",23156762,1092,"Philippe Coutinho","1382894360",false,false,false,618,"https://instagram.ftun9-1.fna.fbcdn.net/v/t51.2885-19/s150x150/69437559_363974237877617_991135940606951424_n.jpg?tp=1&_nc_ht=instagram.ftun9-1.fna.fbcdn.net&_nc_ohc=uiYY_up9lLwAX8rG9wR&edm=ABfd0MgBAAAA&ccb=7-4&oh=c3a24d2609c83e4cf8d017318f3b034e&oe=60CBC5C0&_nc_sid=7bff83","phil.coutinho")
     ).toDF()
