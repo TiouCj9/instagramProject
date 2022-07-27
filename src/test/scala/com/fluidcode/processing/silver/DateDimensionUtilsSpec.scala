f2db9255ad09a28600959c40c29b48454a3c8831
TiouCj9 <ybenmuhamed@icloud.com>
2022-07-27 15:13:26 +0100

    delete useless function

--- src/test/scala/com/fluidcode/processing/silver/DateDimensionUtilsSpec.scala
+++ src/test/scala/com/fluidcode/processing/silver/DateDimensionUtilsSpec.scala
@@ -3,6 +3,7 @@ package com.fluidcode.processing.silver
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+import com.fluidcode.models.DateDim
 import com.fluidcode.processing.silver.DateDimensionUtils._
 import org.apache.spark.sql.delta.test.DeltaExtendedSparkSession
 import org.apache.spark.sql.test.SharedSparkSession
@@ -97,14 +98,25 @@ class DateDimensionUtilsSpec extends QueryTest
     val expectedResult = "true"
     expectedResult contains result
   }
+
   test("createDateDimension Return full date with description and holidays" ) {
+
     val dateFormat = "yyyy-MM-dd"
     val formatter = DateTimeFormatter.ofPattern(dateFormat)
     val startDate = LocalDate.parse("2022-04-17", formatter)
     val endDate = LocalDate.parse("2022-04-19", formatter)
     val result = createDateDimension(spark, startDate,endDate)
-    //Seq(expectedResult).toDF().collect should contain theSameElementsAs (result.collect())
-    result.show()
+    val expectedResult = Seq(
+      DateDim(date = "2022-04-17",description = "SUNDAY, APRIL 17, 2022",dayOfMonth = 17, dayOfWeek = "SUNDAY",
+        month = "APRIL", year = 2022, quarter = "Q2", isWeekend = true, isHoliday = true),
+      DateDim(
+        date = "2022-04-18",description = "MONDAY, APRIL 18, 2022",dayOfMonth = 18, dayOfWeek = "MONDAY",
+        month = "APRIL", year = 2022, quarter = "Q2", isWeekend = false, isHoliday = false),
+      DateDim(
+        date = "2022-04-19",description = "TUESDAY, APRIL 19, 2022",dayOfMonth = 19, dayOfWeek = "TUESDAY",
+        month = "APRIL", year = 2022, quarter = "Q2", isWeekend = false, isHoliday = false)
+    )
+    expectedResult contains result
 
   }
 
