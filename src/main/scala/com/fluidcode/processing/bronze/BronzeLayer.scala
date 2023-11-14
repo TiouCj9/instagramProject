327f5a30307ac96dd6da9d178a97994bfab91d6b
PcImb <chihebbouazizi@gmail.com>
2023-11-14 16:58:53 +0100

    "BronzeLayer and BronzeLayerSpec: read stream using schema, use writeStream to write bronze data, and add option checkpointLocation "

--- src/main/scala/com/fluidcode/processing/bronze/BronzeLayer.scala
+++ src/main/scala/com/fluidcode/processing/bronze/BronzeLayer.scala
@@ -1,19 +1,21 @@
 package com.fluidcode.processing.bronze
 
 import com.fluidcode.configuration.Configuration
+import com.fluidcode.models.bronze.Data
 import org.apache.spark.sql.{Encoders, SparkSession}
 
 class BronzeLayer(conf: Configuration, spark: SparkSession, path: String) {
-
   def createBronzeTable(): Unit = {
 
-    val bronzeData = spark.readStream.option("multiLine", true).json(path)
+    val bronzeSchema = Encoders.product[Data].schema
+    val bronzeData = spark.readStream.schema(bronzeSchema).json(path)
 
     bronzeData
       .writeStream
       .format("delta")
+      .queryName("BronzeTable")
       .outputMode("append")
-      .option("path", s"${conf.rootPath}/${conf.database}/${conf.bronzeTable}")
-      .start()
+      .option("checkpointLocation", "checkpoint_dir")
+      .start(s"${conf.rootPath}/${conf.database}/${conf.bronzeTable}")
   }
 }
--- src/test/scala/com/fluidcode/processing/bronze/BronzeLayerSpec.scala
+++ src/test/scala/com/fluidcode/processing/bronze/BronzeLayerSpec.scala
@@ -1,26 +1,31 @@
 package com.fluidcode.processing.bronze
 
 import com.fluidcode.configuration.Configuration
-import org.apache.spark.sql.QueryTest
+import com.fluidcode.models.bronze.Data
+import org.apache.spark.sql.{Encoders, QueryTest}
 import org.apache.spark.sql.delta.test.DeltaExtendedSparkSession
 import org.apache.spark.sql.test.SharedSparkSession
+
 class BronzeLayerSpec extends QueryTest
   with SharedSparkSession
   with DeltaExtendedSparkSession  {
 
-  test("createBronzeTable should create  BronzeTable from Ingestion Layer" ) {
+  test("createBronzeTable should create BronzeTable from Ingestion Layer" ) {
     withTempDir { dir =>
       val sparkSession = spark
       val conf = Configuration(dir.toString)
       conf.init(sparkSession)
 
-      val path = "phil.coutinho-1-test.json"
+      val path = "c:/tmp/StreamSource/bronze"
+
       val bronzeLayer = new BronzeLayer(conf, sparkSession, path)
       bronzeLayer.createBronzeTable()
       Thread.sleep(5000)
 
       val result = spark.read.format("delta").load(s"${conf.rootPath}/${conf.database}/${conf.bronzeTable}")
-      val expectedResult = spark.read.option("multiLine", true).json(path)
+
+      val bronzeSchema = Encoders.product[Data].schema
+      val expectedResult = spark.read.schema(bronzeSchema).json(path)
       assert(result.except(expectedResult).isEmpty)
     }
   }
