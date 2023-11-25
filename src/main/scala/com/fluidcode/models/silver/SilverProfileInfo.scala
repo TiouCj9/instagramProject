e72d7765cd7fc2113fcf85e8b647d1d8da6f78df
PcImb <chihebbouazizi@gmail.com>
2023-11-25 20:38:15 +0100

    "create profile info table"

---
+++ src/main/scala/com/fluidcode/models/silver/SilverProfileInfo.scala
@@ -0,0 +1,5 @@
+package com.fluidcode.models.silver
+
+case class SilverProfileInfo(created_time: Long, biography: String, followers_count: Long, following_count: Long,
+                             full_name: String, id: String, is_business_account: Boolean, is_joined_recently: Boolean,
+                             is_private: Boolean, posts_count: Long, username: String)
